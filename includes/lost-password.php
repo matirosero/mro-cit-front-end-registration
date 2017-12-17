cb4dd1a4d9fd072b6395f0e204c1d4f715f4d7b1
MatiRosero <gekidasa@gmail.com>
2017-12-17 12:28:50 -0600

    get user info and reset key

--- includes/lost-password.php
+++ includes/lost-password.php
@@ -45,14 +45,14 @@ function mro_cit_lost_password_form_fields() {
 
 			?>
 
-			<form method="POST">
+			<form id="lostpasswordform" action="<?php echo wp_lostpassword_url(); ?>" method="post">
 				<fieldset>
 					<p><?php _e('Please enter your username or email address. You will receive a link to create a new password via email.', 'mro-cit-frontend'); ?></p>
 
 					<p>
-						<label for="pippin_user_login"><?php _e('Username or E-mail', 'mro-cit-frontend'); ?></label>
+						<label for="user_login"><?php _e('Username or E-mail', 'mro-cit-frontend'); ?></label>
 						<?php $user_login = isset( $_POST['user_login'] ) ? $_POST['user_login'] : ''; ?>
-						<input name="pippin_user_login" id="pippin_user_login" class="required" type="text" value="<?php echo $user_login; ?>" />
+						<input name="user_login" id="user_login" class="required" type="text" value="<?php echo $user_login; ?>" />
 					</p>
 
 					<p>
@@ -71,10 +71,106 @@ function mro_cit_lost_password_form_fields() {
 }
 
 function mro_reset_password() {
-	if ( !is_user_logged_in() && isset( $_POST['pippin_user_login'] ) && isset( $_POST['mro_lost_password_nonce'] ) && wp_verify_nonce( $_POST['mro_lost_password_nonce'], 'mro-lost-password-nonce' ) ) {
+	if ( !is_user_logged_in() && isset( $_POST['user_login'] ) && isset( $_POST['mro_lost_password_nonce'] ) && wp_verify_nonce( $_POST['mro_lost_password_nonce'], 'mro-lost-password-nonce' ) ) {
 
-		write_log('Process reset password');
+		write_log('Step 1: reset process starts');
 
+		if ( empty( $_POST['user_login'] ) || ! is_string( $_POST['user_login'] ) ) {
+
+			pippin_errors()->add('empty_username', __('<strong>ERROR</strong>: Enter a username or email address.'));
+			write_log('Empty username');
+
+		} elseif ( strpos( $_POST['user_login'], '@' ) ) {
+			write_log('Step 1.5: This is an email!');
+			$user_data = get_user_by( 'email', trim( wp_unslash( $_POST['user_login'] ) ) );
+			if ( empty( $user_data ) ) {
+				pippin_errors()->add('invalid_email', __('<strong>ERROR</strong>: There is no user registered with that email address.'));
+				write_log('Invalid email! No one with that email');
+			}
+
+		} else {
+			write_log('Step 1.5: this is a username, trim it');
+			$login = trim($_POST['user_login']);
+			$user_data = get_user_by('login', $login);
+			if ( empty( $user_data ) ) {
+				pippin_errors()->add('invalid_username', __('<strong>ERROR</strong>: There is no user registered with that username.'));
+				write_log('Invalid username! No one with that name');
+			}
+		}
+
+		$errors = pippin_errors()->get_error_messages();
+
+		if(empty($errors)) {
+
+			write_log('Step 2: no errors, can move on');
+	
+			// Redefining user_login ensures we return the right case in the email.
+			$user_login = $user_data->user_login;
+			$user_email = $user_data->user_email;
+			$key = get_password_reset_key( $user_data );
+
+			write_log('User login: '.$user_login);
+			write_log('User email: '.$user_email);
+			write_log('Reset key: '.$key);
+
+			if ( is_wp_error( $key ) ) {
+				return $key;
+			}
+
+			if ( is_multisite() ) {
+				$site_name = get_network()->site_name;
+			} else {
+				/*
+				 * The blogname option is escaped with esc_html on the way into the database
+				 * in sanitize_option we want to reverse this for the plain text arena of emails.
+				 */
+				$site_name = wp_specialchars_decode( get_option( 'blogname' ), ENT_QUOTES );
+			}
+
+			write_log('Sitename: '.$site_name);
+		}
+
+
+
+		// $user_login	= sanitize_user( $_POST["user_login"] );
+
+		//https://hitchhackerguide.com/2011/02/12/retrieve_password/
+		//http://www.sutanaryan.com/2011/10/how-to-create-custom-reset-or-forget-password-in-wordpress/
+
+		// require_once(rtrim($_SERVER['DOCUMENT_ROOT'], '/') . '/wp-login.php');
+
+		// $errors = retrieve_password();
+		// do_action( 'lostpassword_post');
+
+		//SEE THIS http://www.sutanaryan.com/2014/08/custom-user-reset-forgot-password-using-ajax-wordpress/
+
+        // if ( is_wp_error( $errors ) ) {
+        //     // Errors found
+        //     $redirect_url = home_url( 'member-password-lost' );
+        //     $redirect_url = add_query_arg( 'errors', join( ',', $errors->get_error_codes() ), $redirect_url );
+        // } else {
+        //     // Email sent
+        //     $redirect_url = home_url( 'member-login' );
+        //     $redirect_url = add_query_arg( 'checkemail', 'confirm', $redirect_url );
+        // }
+
+		//meh
+		// if(!username_exists($user_login)) {
+		// 	// Username already registered
+		// 	pippin_errors()->add('username_unavailable', __('Username does not exist', 'mro-cit-frontend'));
+		// }
+		// if(!validate_username($user_login)) {
+		// 	// invalid username
+		// 	pippin_errors()->add('username_invalid', __('Invalid username', 'mro-cit-frontend'));
+		// }
+		// if($user_login == '') {
+		// 	// empty username
+		// 	pippin_errors()->add('username_empty', __('Please enter a username', 'mro-cit-frontend'));
+		// }
+
+		// $errors = pippin_errors()->get_error_messages();
+
+		if(empty($errors)) {}
 	}
 }
 add_action('init', 'mro_reset_password');
