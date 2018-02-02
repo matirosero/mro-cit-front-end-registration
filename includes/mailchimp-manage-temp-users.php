c67a4e617de99dcdeb8342fff71800b8b7e3a3d9
MatiRosero <gekidasa@gmail.com>
2018-02-01 23:42:40 -0600

    successfully remove emails from list

--- includes/mailchimp-manage-temp-users.php
+++ includes/mailchimp-manage-temp-users.php
@@ -13,22 +13,38 @@ function mro_cit_mailchimp_members_shortcode($atts, $content = null ) {
 
 	if ( is_user_logged_in() && current_user_can( 'manage_temp_subscribers' ) ) {
 
-		$members = mro_cit_get_mailchimp_list_members();
-
-		// Get the queried object and sanitize it
-		$current_page = sanitize_post( $GLOBALS['wp_the_query']->get_queried_object() );
-		// Get the page slug
-		$slug = $current_page->post_name;
-
-		// var_dump($complete_url);
-
-		if (isset($_GET)) {
-			var_dump($_GET['mc_remove']);
+		//Show any messages
+		if ( mro_cit_frontend_messages() != '' ) {
+			echo mro_cit_frontend_messages();
 		}
 
+		$output .= mro_cit_show_temp_members_table();
 
-		if ( count( $members ) > 0 ) {
-			$output .= '<table>
+		$output .= mro_cit_add_temp_member_form();
+
+	} else {
+		$output = '<p class="callout warning">' . __('Your account doesn\'t have permission to see this page.', 'mro-cit-frontend') . '</p>';
+	}
+
+	return $output;
+}
+add_shortcode('cit-mailchimp-members', 'mro_cit_mailchimp_members_shortcode');
+
+
+function mro_cit_show_temp_members_table() {
+
+	$members = mro_cit_get_mailchimp_list_members();
+
+	// Get the queried object and sanitize it
+	$current_page = sanitize_post( $GLOBALS['wp_the_query']->get_queried_object() );
+	// Get the page slug
+	$slug = $current_page->post_name;
+
+	$output = '';
+
+	if ( count( $members ) > 0 ) {
+		$output .= '<h3>Suscriptores temporales</h3>
+			<table>
 				<tr>
 					<th>E-mail</th>
 					<th>Nombre</th>
@@ -37,35 +53,56 @@ function mro_cit_mailchimp_members_shortcode($atts, $content = null ) {
 					<th></th>
 				</tr>';
 
-			foreach ($members as $key => $member) {
+		foreach ($members as $key => $member) {
 
-				$nonce = wp_create_nonce('cit-nonce');
+			$nonce = wp_create_nonce('cit-unsusbcribe-nonce');
 
-				$output .= '<tr>
-					<td>'.$member['email'].'</td>
-					<td>'.$member['fname'].'</td>
-					<td>'.$member['lname'].'</td>
-					<td>'.$member['status'].'</td>
-					<td><a href="/'.$slug.'/?mc_remove='.urlencode($member['email']).'&cit-nonce='.$nonce.'"><i class="icon-cancel"></i></a></td>
-				</tr>';
-			}
-
-			$output .= '</table>';
-		} else {
-			$output = '<p class="callout warning">' . __('Your account doesn\'t have permission to see this page.', 'mro-cit-frontend') . '</p>';
+			$output .= '<tr>
+				<td>'.$member['email'].'</td>
+				<td>'.$member['fname'].'</td>
+				<td>'.$member['lname'].'</td>
+				<td>'.$member['status'].'</td>
+				<td><a href="/'.$slug.'/?mc_remove='.urlencode($member['email']).'&cit-nonce='.$nonce.'"><i class="icon-cancel"></i></a></td>
+			</tr>';
 		}
 
-		$output .= mro_cit_add_temp_member_form();
-
+		$output .= '</table>';
+	} else {
+		$output .= '<p class="callout alert">No hay suscriptores temporales.</p>';
 	}
-
-	
-
-
 
 	return $output;
 }
-add_shortcode('cit-mailchimp-members', 'mro_cit_mailchimp_members_shortcode');
+
+
+function mro_cit_mc_unsubscribe_temp_member() {
+	if ( empty( $_GET ) ) {
+        return false;
+    }
+
+    if ( isset( $_GET['mc_remove'] ) && isset( $_GET['cit-nonce'] ) && wp_verify_nonce($_GET['cit-nonce'], 'cit-unsusbcribe-nonce') ) {
+
+    	write_log('unsubscribe it!');
+
+    	$email = sanitize_email( $_GET['mc_remove'] );
+
+    	if(!is_email($email)) {
+			//invalid email
+			pippin_errors()->add('email_invalid', __('Invalid email', 'mro-cit-frontend'));
+			// write_log('Email error: Invalid email');
+		}
+
+		$errors = pippin_errors()->get_error_messages();
+
+		// only create the user in if there are no errors
+		if(empty($errors)) {
+			mro_cit_unsubscribe_email( $email );
+		}
+
+    	
+    }
+}
+add_action('init', 'mro_cit_mc_unsubscribe_temp_member');
 
 
 function mro_cit_add_temp_member_form() {
@@ -147,9 +184,16 @@ function mro_cit_mc_add_temp_member() {
 
 			// Send to mailchimp function
 			// write_log('MERGE FIELDS: '.implode(",",$mc_merge_fields));
-			mro_cit_subscribe_email($email, $mc_merge_fields, $status);
+			$subscribe = mro_cit_subscribe_email($email, $mc_merge_fields, $status);
 
-			$success_message = 'El correo '.$email.' ha sido agregado a la lista.';
+
+			//Not working
+			if ( $subscribe != false ) {
+				mro_cit_frontend_messages( '<p class="callout success">' . __('The email address '.$email.' has been succesfully subscribed!', 'mro-cit-frontend') . '</p>' );
+			}
+
+
+
 		}
 
     }
