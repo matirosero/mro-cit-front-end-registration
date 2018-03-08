452374254148349ab1d4bc7a7ac9cb8e9c9d5b53
MatiRosero <gekidasa@gmail.com>
2018-03-08 16:34:34 -0600

    approve unapprove works but no email sent

--- includes/frontend-manage-members.php
+++ includes/frontend-manage-members.php
@@ -178,7 +178,7 @@ function cit_approve_member() {
 			$additional_contacts = get_user_meta( $user->ID, 'mro_cit_user_additional_contacts', true );
 
 			if ( $approve === true ) {
-				write_log('Approve it!');
+				// write_log('Approve it!');
 
 				$status = 'subscribed';
 
@@ -190,13 +190,6 @@ function cit_approve_member() {
 				$mc_merge_fields['FNAME'] = $user->user_firstname;
 				$mc_merge_fields['LNAME'] = $user->user_lastname;
 				$mc_merge_fields['EMPRESA'] = $user->nickname;
-
-				// write_log('Phone '.$user->mro_cit_user_phone);
-				// write_log('Country '.$user->mro_cit_user_country);
-				// write_log('Sector '.$user->mro_cit_user_sector);
-				// write_log('First name '.$user->user_firstname);
-				// write_log('Last name '.$user->user_lastname);
-				// write_log('Empresa '.$user->nickname);
 
 				//Check which role
 				if ( members_user_has_role( $user->ID, 'afiliado_institucional_pendiente' ) ) {
@@ -227,7 +220,9 @@ function cit_approve_member() {
 
 				// Send to mailchimp function
 				write_log('Subscribing '.$mc_merge_fields['FNAME'].' '.$mc_merge_fields['LNAME'].' '.$user->user_email);
-				mro_cit_subscribe_email($user->user_email, $mc_merge_fields, $status);
+
+				$subscribe = mro_cit_subscribe_email($user->user_email, $mc_merge_fields, $status);
+				$result['message'] = $subscribe;
 
 				//Get additionals and send to mailchimp
 				if (is_array($additional_contacts)) {
@@ -237,37 +232,66 @@ function cit_approve_member() {
 						$mc_merge_fields['FNAME'] = $contact['name'];
 						$mc_merge_fields['LNAME'] = $contact['lastname'];
 
-						mro_cit_subscribe_email($contact['email'], $mc_merge_fields, $status);
+						$subscribe = mro_cit_subscribe_email($contact['email'], $mc_merge_fields, $status);
+						$result['message'] .= $subscribe;
 
 					}
 				}
 
+				$result['message'] .= '<p class="callout success">'.$user->nickname.' aprobado como Afiliado Empresarial/Institucional</p>';
+
 
 			} else {
-				write_log('Unapprove it!');
+				// write_log('Unapprove it!');
 
 				//Check which role
+				if ( members_user_has_role( $user->ID, 'afiliado_institucional' ) ) {
+
+					// write_log('Is institucional pendiente');
+
+					$old_role = 'afiliado_institucional';
+					$new_role = 'afiliado_institucional_pendiente';
+
+				} elseif ( members_user_has_role( $user->ID, 'afiliado_empresarial' ) ) {
+
+					// write_log('Is empresarial pendiente');
+
+					$mc_merge_fields['AFILIADO'] = 'Empresarial';
+
+					$old_role = 'afiliado_empresarial';
+					$new_role = 'afiliado_empresarial_pendiente';
+
+				}
 
 				//Change role
+				$user->remove_role( $old_role );
+				$user->add_role( $new_role );
 
 				//Get mail email and unsubscribe from mailchimp
+				$unsubscribe = mro_cit_unsubscribe_email( $user->user_email );
+				$result['message'] = $unsubscribe;
 
 				//Get additionals and unsubscribe from mailchimp
+				if (is_array($additional_contacts)) {
+
+					foreach ($additional_contacts as $contact) {
+						write_log('Unsubscribing additional '.$contact['name'].' '.$contact['lastname'].' '.$contact['email']);
+
+						$unsubscribe = mro_cit_unsubscribe_email( $contact['email'] );
+						$result['message'] .= $unsubscribe;
+
+					}
+				}
+
+				$result['message'] .= '<p class="callout success">'.$user->nickname.' pendiente de aprobación.</p>';
 			}
-
-
-
-    		
-
-
-
-
+			$result['type'] = 'success';
 
 		} else {
 			$result['type'] = 'error';
 			$result['message'] = $errors;
 		}
-/*
+
 		if(!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
 	      	// $result['re place'] = mro_cit_build_temp_subscribers_table();
 	      	$result = json_encode($result);
@@ -277,7 +301,7 @@ function cit_approve_member() {
 		} else {
 		    header("Location: ".$_SERVER["HTTP_REFERER"]);
 		}
-*/
+
 
     } else {
     	// write_log('NOT LOGGED IN');
